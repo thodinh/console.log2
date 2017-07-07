cfa55d90b4f6d5d6469b97110cc2ec81ff80aaf1
thodinh <d.tr.tho@gmail.com>
2017-07-07 23:41:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-var Log2 = function(context) {
-    Object.defineProperty(global, '__stack', {
-        get: function() {
-            var orig = Error.prepareStackTrace;
-            Error.prepareStackTrace = function(_, stack) {
-                return stack;
-            };
-            var err = new Error;
-            Error.captureStackTrace(err, arguments.callee);
-            var stack = err.stack;
-            Error.prepareStackTrace = orig;
-            return stack;
-        }
-    });
+var getStackTrace = function() {
+    // var obj = {};
+    // Error.captureStackTrace(obj, getStackTrace);
+    var orig = Error.prepareStackTrace;
+    Error.prepareStackTrace = function(_, stack) {
+        // console.log(stack.toString());
+        return stack;
+    };
+    var err = new Error;
+    Error.captureStackTrace(err, getStackTrace);
+    var stack = err.stack;
+    Error.prepareStackTrace = orig;
+    return stack && stack[1] && stack[1].toString();
+};
 
-    Object.defineProperty(global, '__loginfo', {
-        get: function() {
-            var f = __stack[1].getFunctionName();
-            return __filename + (f ? ' > ' + f + ':' : ':') + __stack[1].getLineNumber() + ',';
-        }
-    });
+var Log2 = function(context) {
+    // Object.defineProperty(global, '__stack', {
+    //     get: function() {
+    //         var orig = Error.prepareStackTrace;
+    //         Error.prepareStackTrace = function(_, stack) {
+    //             return stack;
+    //         };
+    //         var err = new Error;
+    //         Error.captureStackTrace(err, arguments.callee);
+    //         var stack = err.stack;
+    //         Error.prepareStackTrace = orig;
+    //         return stack;
+    //     }
+    // });
+
+    // Object.defineProperty(global, '__loginfo', {
+    //     get: function() {
+    //         var f = __stack[1].getFunctionName();
+    //         return __filename + (f ? ' > ' + f + ':' : ':') + __stack[1].getLineNumber() + ',';
+    //     }
+    // });
 
     function getLogDate() {
         //Timezone is 0;
@@ -29,26 +44,29 @@ var Log2 = function(context) {
 
     var log = console.log;
     console.log = function () {
-        log.apply(console, [`${getLogDate()},`, 'DEBUG,', ...arguments]);
+        var trace = getStackTrace() + ",";
+        log.apply(console, [`${getLogDate()},`, 'DEBUG,', trace, ...arguments]);
     }
 
-    var error = console.error;
+    // var error = console.error;
     console.error = function () {
-        error.apply(console, [`${getLogDate()},`, 'ERROR,', ...arguments]);
+        console.trace.apply(console, [`${getLogDate()},`, 'ERROR,', ...arguments]);
     }
 
     console.fatal = function () {
-        error.apply(console, [`${getLogDate()},`, 'FATAL,', ...arguments]);
+        console.trace.apply(console, [`${getLogDate()},`, 'FATAL,', ...arguments]);
     }
 
     var info = console.info;
     console.info = function () {
-        info.apply(console, [`${getLogDate()},`, 'INFO,', ...arguments]);
+        var trace = getStackTrace() + ",";
+        info.apply(console, [`${getLogDate()},`, 'INFO,', trace, ...arguments]);
     }
 
     var warn = console.warn;
     console.warn = function () {
-        warn.apply(console, [`${getLogDate()},`, 'WARN,', ...arguments]);
+        var trace = getStackTrace() + ",";
+        warn.apply(console, [`${getLogDate()},`, 'WARN,', trace, ...arguments]);
     }
     return this;
 }
